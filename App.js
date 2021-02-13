f4f45780f53c1b334bb80f89c13fceaba16063d7
Poyao Wang <michael90110@gmail.com>
2021-02-13 16:24:21 +0900

    Refactor timer to App.js

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+
 import Timer from "./app/Timer";
 
 export default function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
   return (
     <View style={styles.container}>
-      <Timer />
+      <Timer seconds={seconds} />
+      <Button
+        title={isActive ? "active" : "inactive"}
+        onPress={toggle}
+      ></Button>
+      <Button onPress={reset} title="Reset"></Button>
     </View>
   );
 }
--- app/Timer.js
+++ app/Timer.js
@@ -1,41 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import React from "react";
 
-function Timer(props) {
-  const [seconds, setSeconds] = useState(0);
-  const [isActive, setIsActive] = useState(false);
-
-  function toggle() {
-    setIsActive(!isActive);
-  }
-
-  function reset() {
-    setSeconds(0);
-    setIsActive(false);
-  }
-
-  useEffect(() => {
-    let interval = null;
-    if (isActive) {
-      interval = setInterval(() => {
-        setSeconds((seconds) => seconds + 1);
-      }, 1000);
-    } else if (!isActive && seconds !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, seconds]);
-
-  return (
-    <View style={styles.container}>
-      <Text>{seconds}</Text>
-      <Button
-        title={isActive ? "active" : "inactive"}
-        onPress={toggle}
-      ></Button>
-      <Button onPress={reset} title="Reset"></Button>
-    </View>
-  );
+function Timer({ seconds }) {
+  return <Text>{seconds}</Text>;
 }
 
 const styles = StyleSheet.create({
